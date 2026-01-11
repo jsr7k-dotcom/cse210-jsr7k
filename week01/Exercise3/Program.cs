7c2549ac6c6138b58e8d47771f04f0b87e9a7f9c
jsr7k-dotcom <jhernandezpea@byupathway.edu>
2026-01-10 23:48:30 -0600

    Third exercise completed.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int userGuess = -1;
+            int guessCount = 1;
+
+            while (userGuess != magicNumber)
+            {
+                Console.WriteLine("Guess a number between 1 and 100");
+                userGuess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You win!");
+                    Console.WriteLine($"It took you {guessCount} times to guess.");
+                }
+            }
+            Console.WriteLine("Do you want to play again?y/n");
+            playAgain = Console.ReadLine().ToLower();
+        }while (playAgain == "y");
     }
 }
