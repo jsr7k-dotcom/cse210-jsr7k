a5d063c8b533415f9e3ea8b87e5cf164f293dece
jsr7k-dotcom <jhernandezpea@byupathway.edu>
2026-01-10 19:58:51 -0600

    Second exercise completed.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter the grade percentage:");
+        string input = Console.ReadLine();
+
+        int gradePercentage = int.Parse(input);
+
+        string gradeLetter;
+
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        string gradeSign;
+        int lastDigit = gradePercentage % 10;
+
+        if (lastDigit >= 7)
+        {
+            gradeSign = "+";
+        }
+        else if (lastDigit <3)
+        {
+            gradeSign = "-";
+        }
+        else
+        {
+            gradeSign = "";
+        }
+
+        if (gradeLetter == "A" && lastDigit < 3)
+        {
+            gradeSign = "-";
+        }
+        else if (gradeLetter == "A" && lastDigit >= 3)
+        {
+            gradeSign = "";
+        }
+        else if (gradeLetter == "F")
+        {
+            gradeSign = "";
+        }
+
+
+        Console.WriteLine($"Your letter grade is {gradeLetter}{gradeSign}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you approved the class.");
+        }
+        else
+        {
+            Console.WriteLine("You need to work harder, the luck is not required in this class.");
+        }
     }
 }
