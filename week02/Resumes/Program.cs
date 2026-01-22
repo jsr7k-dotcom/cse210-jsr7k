b8fd4b4037d9874da3d2f8bb588b99ae6a07b060
jsr7k-dotcom <jhernandezpea@byupathway.edu>
2026-01-22 01:50:47 -0600

    Resume exercise completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Psychologist";
+        job1._company = "Seattle Grace Hospital";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "Psychologist";
+        job2._company = "Grey-Sloan Memorial Hospital";
+        job2._startYear = "2022";
+        job2._endYear = "2025";
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Lenina Balderas";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.DisplayList();
     }
 }
