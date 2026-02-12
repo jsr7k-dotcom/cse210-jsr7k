5027e4bae6a4043965736663c73d86bad4675acf
jsr7k-dotcom <jhernandezpea@byupathway.edu>
2026-02-12 03:57:21 -0600

    Learning activity completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Charles Brown", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment math1 = new MathAssignment("Sheldon Cooper", "Fractions", "6.8", "7-18");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writing1 = new WritingAssignment("Leonard Hoftstader", "European History", "The Causes of World War II");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
