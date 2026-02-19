3328e6ea23897d5b25fd7292df424cd317ad7b53
jsr7k-dotcom <jhernandezpea@byupathway.edu>
2026-02-19 05:00:01 -0600

    chore: update code structure for better readability and maintainability

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        Running run = new Running("07 Nov 2025", 30, 4.8);
+        Cycling cycle = new Cycling("15 Nov 2025", 45, 20.0);
+        Swimming swim = new Swimming("30 Nov 2025", 25, 40);
+
+        List<Activity> activities = new List<Activity>();
+        activities.Add(run);
+        activities.Add(cycle);
+        activities.Add(swim);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
